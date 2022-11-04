5445aa1bfd61b6c406939cc0b322cfa62dd8009b
jia yi <jiayi.ng@digipen.edu>
2022-11-04 15:45:25 +0800

    Testing audio in scene

---
+++ UXG3503_GameDesign3/Assets/Scripts/Curtain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curtain : MonoBehaviour
+{
+
+    //AudioScript reference
+    private AudioScript audioRef;
+    public float delayTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioRef = GetComponent<AudioScript>();
+        audioRef.playAudio();
+        StartCoroutine("DelayAudioCoroutine");
+    }
+
+    IEnumerator DelayAudioCoroutine()
+    {
+        yield return new WaitForSeconds(delayTime);
+        audioRef.playAudio2();
+    }
+}
