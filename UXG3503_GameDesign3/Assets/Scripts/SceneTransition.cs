6e36edd162c44ee782032f3ffa0ad4716e960a04
jia yi <jiayi.ng@digipen.edu>
2022-11-10 16:12:53 +0800

    sounds in menu

--- UXG3503_GameDesign3/Assets/Scripts/SceneTransition.cs
+++ UXG3503_GameDesign3/Assets/Scripts/SceneTransition.cs
@@ -7,10 +7,14 @@ public class SceneTransition : MonoBehaviour
 
 
     private Animator animRef;
+    public AudioScript audioQuitRef;
+    public AudioScript audioPlayRef;
 
     private void Start()
     {
         animRef = GetComponent<Animator>();
+        audioQuitRef = GetComponent<AudioScript>();
+        audioPlayRef = GetComponent<AudioScript>();
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
@@ -18,12 +22,12 @@ public class SceneTransition : MonoBehaviour
 
     public void NextScene()
     {
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        StartCoroutine(PostBtnAudio(4.0f));
         Application.Quit();
     }
 
@@ -34,6 +38,26 @@ public class SceneTransition : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         animRef.SetTrigger("Transition");
+        // puppet moving down the screen 2.5s
+        // 6 sticks moving
+        audioPlayRef.playAudio3();
+        audioPlayRef.playAudio4();
+        // @1.3s play curtain sounds
+        StartCoroutine(CloseCurtainAudio(1.3));
+        
     }
+
+    IEnumerator PostBtnAudio(float waitTime)
+    {
+        audioQuitRef.playAudio3();
+        yield return new WaitForSeconds(waitTime);
+    }
+
+    IEnumerator CloseCurtainAudio(float waitTime)
+    {
+        audioQuitRef.playAudio3();
+        yield return new WaitForSeconds(waitTime);
+    }
+
 
 }
