a392a61841bad1703fdf6af7c7697906f2a8eefa
jia yi <jiayi.ng@digipen.edu>
2022-11-13 17:00:39 +0800

    Scene related audio

---
+++ UXG3503_GameDesign3/Assets/AudioBGM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioBGM : MonoBehaviour
+{
+    private AudioSource AudioRef;
+
+    private void Awake()
+    {
+        // Do not destroy audio track
+        DontDestroyOnLoad(transform.gameObject);
+        AudioRef = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //reach main screne fade out
+    }
+}
