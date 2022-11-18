ffbc5dfba03a7577b38ed194f03e1845abb172d1
dragonsister199 <93319554+nicoletlw@users.noreply.github.com>
2022-11-18 22:22:50 +0800

    test code

---
+++ UXG3503_GameDesign3/Assets/Scripts/TutWitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutWitch : MonoBehaviour
+{
+    private Animator animRef;
+
+    private bool tutPhase1 = false;
+    private bool tutPhase2 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
